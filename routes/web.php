84760187c06793ca64e614518507b5fd77d1cafc
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2026-08-07 16:34:24 -0300

    route site permission

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
-Route::get('/site', [SiteController::class, 'index'])->name('site.index');
-Route::get('/site/conteudos', [SiteController::class, 'contentIndex'])->name('site.content.index');
-Route::get('/site/noticias', [SiteController::class, 'newsIndex'])->name('site.news.index');
-Route::get('/site/noticias/{news}', [SiteController::class, 'newsShow'])->name('site.news.show');
-Route::get('/site/eventos', [SiteController::class, 'eventsIndex'])->name('site.events.index');
-Route::get('/site/eventos/{event}', [SiteController::class, 'eventsShow'])->name('site.events.show');
+Route::middleware('auth')->group(function (): void {
+    Route::get('/site', [SiteController::class, 'index'])->name('site.index');
+    Route::get('/site/conteudos', [SiteController::class, 'contentIndex'])->name('site.content.index');
+    Route::get('/site/noticias', [SiteController::class, 'newsIndex'])->name('site.news.index');
+    Route::get('/site/noticias/{news}', [SiteController::class, 'newsShow'])->name('site.news.show');
+    Route::get('/site/eventos', [SiteController::class, 'eventsIndex'])->name('site.events.index');
+    Route::get('/site/eventos/{event}', [SiteController::class, 'eventsShow'])->name('site.events.show');
+});
